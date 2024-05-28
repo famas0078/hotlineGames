05ece9e4ba3397fc0e63d7be2e9d5b7792d88c3a
Yason <nazardenr@mail.ru>
2024-05-28 17:04:01 +0300

    add new app

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="wrapper-header">
+    <div class="left-content">
+      <div class="logo-item">
+        <div class="logo-icon">
+          <img src="../assets/images/logo.svg" alt="">
+        </div>
+        <div class="logo-title">
+          <h1 class="logo-title-left mt-5">
+            Hotline
+          </h1>
+          <h2 class="logo-title-right">
+            Gaming
+          </h2>
+        </div>
+      </div>
+    </div>
+    <div class="right-content">
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
